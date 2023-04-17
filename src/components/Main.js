74e39627c894423effd3a58be144962a7d037182
winkevinzzzz <thean.tep.tt@gmail.com>
2023-04-17 22:38:07 +0700

    pull

---
+++ src/components/Main.js
@@ -0,0 +1,275 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+
+<section >
+  <div class="container py-5">
+    <h1 class="text-center mb-5"><strong>Product listing</strong></h1>
+    
+      
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded ">
+          <img src="https://quietrev.com/wp-content/uploads/2017/08/popular-banner.jpg"
+            class="w-100" />
+          <a href="#!">
+            <div class="mask" >
+              <div class="d-flex justify-content-start align-items-start h-100 d-flex justify-content-center">
+                <h3 ><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">Popular category</span></h3>
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask" ></div>
+            </div>
+          </a>
+        </div>
+      
+      
+    <div class="row">
+      <div class="col-lg-4 col-md-12 mb-4">
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(1).webp"
+            class="w-100" />
+          <a href="#!">
+            <div class="mask" >
+              <div class="d-flex justify-content-start align-items-start h-100 d-flex justify-content-center">
+                <h3><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">Fashion</span></h3>
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask" ></div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://images.unsplash.com/photo-1559723944-6913027cf19a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGFibGV0JTIwYW5kJTIwcGhvbmV8ZW58MHx8MHx8&w=1000&q=80"
+            class="w-100" />
+          <a href="#!">
+            <div class="mask" >
+              <div class="d-flex justify-content-start align-items-start h-100 d-flex justify-content-center">
+                <h3><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">Phones & Tabets</span></h3>
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://bleuwire.com/wp-content/uploads/2019/03/laptop-accessories.jpg"
+            class="w-100" />
+          <a href="#!">
+            <div class="mask" >
+              <div class="d-flex justify-content-start align-items-start h-100 d-flex justify-content-center">
+                <h3><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">Computers & Accessories</span></h3>
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask" ></div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-12 mb-4">
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://www.eatingwell.com/thmb/m5xUzIOmhWSoXZnY-oZcO9SdArQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_291139_the-top-10-healthiest-foods-for-kids_-02-4b745e57928c4786a61b47d8ba920058.jpg"
+            class="w-100" />
+          <a href="#!">
+            <div class="mask" >
+              <div class="d-flex justify-content-start align-items-start h-100 d-flex justify-content-center">
+                <h3><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">Foods</span></h3>
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask" ></div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://www.gpb.org/sites/default/files/styles/flexheight/public/blogs/images/2017/10/23/in_demand_jobs.jpg?itok=ziUuhVyp"
+            class="w-100" />
+          <a href="#!">
+            <div class="mask" >
+              <div class="d-flex justify-content-start align-items-start h-100 d-flex justify-content-center">
+                <h3><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">Jobs</span></h3>
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://www.salesforce.com/content/dam/blogs/ca/Blog%20Posts/Every-Task-a-Customer-Service-Rep-Must-Do-opengraph.png"
+            class="w-100" />
+          <a href="#!">
+            <div class="mask" >
+              <div class="d-flex justify-content-start align-items-start h-100 d-flex justify-content-center">
+                <h3><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">Services</span></h3>
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask" ></div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
+  {/* //Feedback */}
+  {/* fill feedback */}
+  <form >
+    <div class="pl-5 pr-5">
+  <h1 class="text-center mb-5"><strong>Welcome your Feedback</strong></h1>
+  <div class="form-group m-5">
+    <label for="exampleFormControlInput1">@Username</label>
+    <input type="username" class="form-control" id="exampleFormControlInput1" placeholder="@Username"/>
+  </div>
+  <div class="form-group m-5">
+    <label for="exampleFormControlSelect1">kindly rate Us</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Very Good</option>
+      <option>Good</option>
+      <option>none</option>
+      <option>bad</option>
+      <option>Very bad</option>
+    </select>
+  </div>
+  <div class="form-group m-5">
+    <label for="exampleFormControlTextarea1">Your Feedback</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="write down here"></textarea>
+  </div>
+  <div class="d-flex justify-content-center mt-5 mb-5"><button type="submit" class="btn btn-primary">Submit</button></div>
+  </div>
+</form>
+{/* end fill feedback */}
+{/* show feedback */}
+<section class="team-section py-5 bg-light">
+        <div class="container">
+          <h1 class="main-title d-flex text-center justify-content-center pb-5">
+            Our Feedback
+          </h1>
+          <div class="row justify-content-center pt-3">
+          <div
+              class="col-12 card-container col-md-6 col-lg-4"
+              data-aos="zoom-in"
+              data-aos-delay="100"
+            >
+              <div class="card border-0  my-5 mx-2 position-relative">
+                <div class="card-body p-4">
+        
+                  <div class="card-text pt-1">
+                    <h5 class="member-name text-center">@VICHHAI</h5>
+                    <p class="text-center  bg-light pt-2 text-dark   ">like</p>
+                    <div class="text-center">
+                      like this i can buy anything i want
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        
+            <div
+              class="col-12 card-container col-md-6 col-lg-4"
+              data-aos="zoom-in"
+              data-aos-delay="100"
+            >
+              <div class="card border-0  my-5 mx-2 position-relative">
+                <div class="card-body p-4">
+                  
+                  <div class="card-text pt-1">
+                    <h5 class="member-name text-center ">@Thairong</h5>
+                    <p class="text-center  bg-light pt-2 text-dark ">none</p>
+                    <div class="text-center">
+                      i found something on this
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+         
+            <div
+              class="col-12 card-container col-md-6 col-lg-4"
+              data-aos="zoom-in"
+              data-aos-delay="100"
+            >
+              <div class="card border-0  my-5 mx-2 position-relative">
+                <div class="card-body p-4">
+                  
+                  <div class="card-text pt-1">
+                    <h5 class="member-name text-center">@Jessica</h5>
+                    <p class="text-center   bg-light pt-2 text-dark">Very like</p>
+                    <div class="text-center">
+                      I want it I got it
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              class="col-12 card-container col-md-6 col-lg-4"
+              data-aos="zoom-in"
+              data-aos-delay="100"
+            >
+              <div class="card border-0  my-5 mx-2 position-relative">
+                <div class="card-body p-4">
+                  
+                  <div class="card-text pt-1">
+                    <h5 class="member-name text-center">@Theanzzzz</h5>
+                    <p class="text-center  bg-light pt-2 text-dark">Like</p>
+                    <div class="text-center">
+                      like it love it
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              class="col-12 card-container col-md-6 col-lg-4"
+              data-aos="zoom-in"
+              data-aos-delay="100"
+            >
+              <div class="card border-0  my-5 mx-2 position-relative">
+                <div class="card-body p-4">
+                  
+                  <div class="card-text pt-1">
+                    <h5 class="member-name text-center">@Ponareachh</h5>
+                    <p class="text-center  bg-light pt-2 text-dark">Very Bad</p>
+                    <div class="text-center">
+                      Dislike it 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+{/* end shhow feedback */}
+
+
+
+</section>
+    </div>
+  )
+}
+
+export default Main
